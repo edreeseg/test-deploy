e560c7162469d0bb210e9b8b7c7557a5a07a1486
Ed Reeseg <edward.reeseg@gmail.com>
2022-07-15 19:36:17 -0500

    lmao not actually static.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -59,6 +60,7 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
